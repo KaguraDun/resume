8591a37fd4220f781f55d15ac91b0ebc7d3715d4
Vasily Kovnev <v.k95@yandex.ru>
2021-09-26 21:37:23 +0300

    feat: add about page

---
+++ src/pages/About/About.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import s from './About.scss';
+
+function About() {
+  return (
+    <div className={s.about}>
+      Hello my name is Kovnev Vasily i am Junior front-end developer. 
+      <hr />
+      <h3>Education</h3>
+      <p />
+    </div>
+  );
+}
+
+export default About;
