bc487c02ec2fc7681ef1a1f7533e2dc1cdf9af2a
Vasily Kovnev <v.k95@yandex.ru>
2021-09-26 21:32:17 +0300

    feat: add footer component

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import s from './Footer.scss';
+
+function Footer() {
+  return <footer className={s.footer}> Vasily Kovnev - 2021</footer>;
+}
+
+export default Footer;
