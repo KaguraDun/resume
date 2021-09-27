3bd166e0ff47476c28e46a00de5c0dc78d4977ed
Vasily Kovnev <v.k95@yandex.ru>
2021-09-27 18:50:45 +0300

    feat: add projects page

--- src/pages/Projects/Projects.tsx
+++ src/pages/Projects/Projects.tsx
@@ -1,9 +1,22 @@
 import React from 'react';
 
+import Project from '@/components/Project/Project';
+import { projects } from '@/models/projects';
+
 import s from './Projects.scss';
 
 function Projects() {
-  return <div className={s.projects}>My projects</div>;
+  return (
+    <div className={s.projects}>
+      {projects.map((item) => (
+        <ul className={s.itemList}>
+          <li className={s.item}>
+            <Project item={item} />
+          </li>
+        </ul>
+      ))}
+    </div>
+  );
 }
 
 export default Projects;
