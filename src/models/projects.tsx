f62ad9a623bbbb3369070277ccd64ca1fd7fca61
Vasily Kovnev <v.k95@yandex.ru>
2021-09-30 01:24:52 +0300

    refactor: change about property type
    update project about

--- src/models/projects.tsx
+++ src/models/projects.tsx
@@ -1,10 +1,12 @@
 /* eslint-disable global-require */
+import React from 'react';
+
 interface ProjectInterface {
   thumbnail: string;
   name: string;
   deploy: string;
   git: string;
-  about: string;
+  about: JSX.Element;
 }
 
 const projects: ProjectInterface[] = [
@@ -13,12 +15,27 @@ const projects: ProjectInterface[] = [
     name: 'Songster clone',
     deploy: 'https://kaguradun.github.io/songster-clone/',
     git: 'https://github.com/KaguraDun/songster-clone',
-    about: `Songster-clone is a clone of popular site with guitar tablatures: songsterr.
-     App created as final task of RSSchool course in team of 4 members.
-     But with additional functions.
-     You can upload any midi file, our app convert it to five line staff and then you watch notes and listen music.
-     You can change instruments and print staff.
-     Also it has a library of already uploaded compositions.`,
+    about: (
+      <>
+        Songster-clone is a clone of popular site with guitar tablatures:
+        songsterr. App created as final task of RSSchool JavaScript/Front-end
+        course in team of 4 members. Main features:
+        <br />
+        <ul>
+          <li>
+            You can upload any midi file, our app convert it to five line staff
+            and then you watch notes and listen music.
+          </li>
+          <li>
+            You can upload any midi file, our app convert it to five line staff
+            and then you watch notes and listen music.
+          </li>
+          <li>You can change instruments and print staff.</li>
+          <li>Also it has a library of already uploaded compositions.</li>
+        </ul>
+      </>
+    ),
+  },
   },
 ];
 
