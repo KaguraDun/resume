1d42cf44c144b646bb44e432a8e32c508856728a
Vasily Kovnev <54976219+KaguraDun@users.noreply.github.com>
2022-03-10 19:56:53 +0300

    feat: update projects description

--- src/models/projects.tsx
+++ src/models/projects.tsx
@@ -21,23 +21,23 @@ const projects: ProjectInterface[] = [
     git: 'https://github.com/KaguraDun/songster-clone',
     about: (
       <>
-        Songster-clone is a clone of popular site with guitar tablatures:
-        songsterr. App created as final task of RSSchool JavaScript/Front-end
-        course in team of 4 members. Main features:
-        <br />
+        <p>
+          Songster-clone is a clone of popular site with guitar tablatures:
+          songsterr.
+        </p>
+        <p>
+          App created as final task of RSSchool JavaScript/Front-end course in
+          team of 4 members.
+        </p>
+        <p>I worked at:</p>
         <ul>
-          <li>
-            You can upload any midi file, our app convert it to five line staff
-            and then you watch notes and listen music.
-          </li>
-          <li>You can change instruments and print staff.</li>
-          <li>Also it has a library of already uploaded compositions.</li>
+          <li>User interface;</li>
+          <li>Music player functionality;</li>
         </ul>
       </>
     ),
   },
   {
-    thumbnail: require('../../public/images/projects/songster-clone/1.png'),
     thumbnail: require('../../public/images/projects/covid-19-dashboard/1.png'),
     stackIcons: [stack.js, stack.sass],
     name: 'Covid 19 dashboard',
@@ -46,10 +46,17 @@ const projects: ProjectInterface[] = [
     git: 'https://github.com/Zavrazhneva/covid-19',
     about: (
       <>
-        Interactive map with various covid statistics and its visualization.
-        <br />
-        App created during RSSchool JavaScript/Front-end course in team of 4
-        members
+        <p>
+          Interactive map with various covid statistics and its visualization.
+        </p>
+        <p>
+          App created during RSSchool JavaScript/Front-end course in team of 4
+          members.
+        </p>
+        <p>I worked at:</p>
+        <ul>
+          <li>Data visualization by Chart.js;</li>
+        </ul>
       </>
     ),
   },
@@ -62,13 +69,14 @@ const projects: ProjectInterface[] = [
     git: 'https://github.com/KaguraDun/rolling-scopes-school/tree/rs-css/rs-css/webpack-boilerplate-master',
     about: (
       <>
-        Interactive css trainer. The simulator consists of several game levels.
-        Each level includes an example of the layout and html code corresponding
-        to the current level. Some elements of the layout are highlighted with
-        animation. It is up to the user to write a CSS selector that matches all
-        of the selected layout elements.
-        <br />
-        App created during RSSchool JavaScript/Front-end course
+        <p>
+          Interactive css trainer. The simulator consists of several game
+          levels. Each level includes an example of the layout and html code
+          corresponding to the current level. Some elements of the layout are
+          highlighted with animation. It is up to the user to write a CSS
+          selector that matches all of the selected layout elements.
+        </p>
+        <p>App created during RSSchool JavaScript/Front-end course</p>
       </>
     ),
   },
@@ -81,73 +89,73 @@ const projects: ProjectInterface[] = [
     git: 'https://github.com/KaguraDun/rolling-scopes-school/tree/gem-puzzle/gem-puzzle/webpack-boilerplate-master',
     about: (
       <>
-        Gem puzzle with different field sizes and best results table.
-        <br />
-        App created during RSSchool JavaScript/Front-end course
+        <p>Gem puzzle with different field sizes and best results table.</p>
+        <p>App created during RSSchool JavaScript/Front-end course</p>
       </>
     ),
   },
   {
     thumbnail: require('../../public/images/projects/virtual-keyboard/1.png'),
-    stackIcons: [stack.js],
+    stackIcons: [stack.js, stack.css],
     name: 'Virtual keyboard',
     deploy:
       'https://rolling-scopes-school.github.io/kaguradun-JS2020Q3/virtual-keyboard/',
     git: 'https://github.com/KaguraDun/rolling-scopes-school/tree/virtual-keyboard/virtual-keyboard',
     about: (
       <>
-        Virtual keyboard with speech recognition and awesome voice acting.
-        <br />
-        App created during RSSchool JavaScript/Front-end course
+        <p>
+          Virtual keyboard with speech recognition and awesome voice acting.
+        </p>
+        <p>App created during RSSchool JavaScript/Front-end course</p>
       </>
     ),
   },
   {
     thumbnail: require('../../public/images/projects/shelter/1.png'),
-    stackIcons: [],
+    stackIcons: [stack.css],
     name: 'Shelter',
     deploy:
       'https://rolling-scopes-school.github.io/kaguradun-JS2020Q3/shelter/pages/main/main.html',
     git: 'https://github.com/KaguraDun/rolling-scopes-school/tree/shelter',
     about: (
       <>
-        Simple two page website
-        <br />
-        App created during RSSchool JavaScript/Front-end course
+        <p>Simple two page website.</p>
+        <p>App created during RSSchool JavaScript/Front-end course.</p>
       </>
     ),
   },
   {
     thumbnail: require('../../public/images/projects/momentum/1.png'),
-    stackIcons: [stack.js],
+    stackIcons: [stack.js, stack.css],
     name: 'Momentum',
     deploy:
       'https://rolling-scopes-school.github.io/kaguradun-JS2020Q3/momentum/',
     git: 'https://github.com/KaguraDun/rolling-scopes-school/tree/momentum',
     about: (
       <>
-        Momentum is an analogue of the app of the same name in the Chrome web
-        store. The application shows the time and username, his goal for the
-        current day. The background image changes depending on the time of day.
-        It also features quotes from famous people and the weather forecast.
-        <br />
-        App created during RSSchool JavaScript/Front-end course
+        <p>
+          Momentum is an analogue of the app of the same name in the Chrome web
+          store. The application shows the time and username, his goal for the
+          current day. The background image changes depending on the time of
+          day. It also features quotes from famous people and the weather
+          forecast.
+        </p>
+        <p>App created during RSSchool JavaScript/Front-end course.</p>
       </>
     ),
   },
   {
     thumbnail: require('../../public/images/projects/calculator/1.png'),
-    stackIcons: [stack.js],
+    stackIcons: [stack.js, stack.css],
     name: 'Calculator',
     deploy:
       'https://rolling-scopes-school.github.io/kaguradun-JS2020Q3/calculator/',
     git: 'https://github.com/KaguraDun/rolling-scopes-school/tree/calculator',
     about: (
-      <>
-        Calculator with basic functions.
-        <br />
-        App created during RSSchool JavaScript/Front-end course
-      </>
+      <p>
+        Calculator with basic functions. App created during RSSchool
+        JavaScript/Front-end course.
+      </p>
     ),
   },
 ];
