373a32d43f1b6b5b45d36f609fb8c5d0718aa543
Vasily Kovnev <54976219+KaguraDun@users.noreply.github.com>
2023-03-25 23:28:46 +0300

    feat: Add linkedin social refactor: Move socials to separate model refactor: Remove Contacts logic from Socials component

--- src/components/Contacts/Contacts.tsx
+++ src/components/Contacts/Contacts.tsx
@@ -1,14 +1,24 @@
 import cn from 'classnames';
 import React from 'react';
 
+import Tooltip from '@/components/Tooltip/Tooltip';
+import GmailLogo from '@/icons/socials/gmail.svg';
+import WhatsappLogo from '@/icons/socials/whatsapp.svg';
 import avatar from '@/images/avatar.png';
 
 import Socials from '../Socials/Socials';
 import s from './Contacts.scss';
 
 function Contacts() {
+  const copyDataToBuffer = (clickEvent: React.MouseEvent) => {
+    const target = clickEvent.target as HTMLInputElement;
+    const data = target.value;
+
+    navigator.clipboard.writeText(data);
+  };
+
   return (
-    <article className={s.contacts}>
+    <article className={s.contactsWrapper}>
       <img alt="avatar" className={s.photo} src={avatar} />
 
       <h1 className={cn(s.title, 'text-heading-h1')}>
@@ -18,6 +28,50 @@ function Contacts() {
           Front-end developer
         </span>
       </h1>
+
+      <address className={s.contacts}>
+        <span className={s.location}>Russia, Voronezh</span>
+
+        <div className={s.contact}>
+          <a
+            className={s.contactLink}
+            href="mailto:vasily.kovnev@gmail.com"
+            rel="noreferrer"
+            target="_blank"
+          >
+            <GmailLogo height="30px" width="30px" />
+          </a>
+          <Tooltip text="copy">
+            <input
+              className={s.contactField}
+              onClick={copyDataToBuffer}
+              readOnly
+              type="text"
+              value="vasily.kovnev@gmail.com"
+            />
+          </Tooltip>
+        </div>
+
+        <div className={s.contact}>
+          <a
+            className={s.contactLink}
+            href="tel:+79507653267"
+            rel="noreferrer"
+            target="_blank"
+          >
+            <WhatsappLogo height="35px" width="35px" />
+          </a>
+          <Tooltip text="copy">
+            <input
+              className={s.contactField}
+              onClick={copyDataToBuffer}
+              readOnly
+              type="text"
+              value="+79507653267"
+            />
+          </Tooltip>
+        </div>
+      </address>
 
       <Socials />
     </article>
--- src/components/Socials/Socials.tsx
+++ src/components/Socials/Socials.tsx
@@ -1,89 +1,25 @@
 import React from 'react';
 
-import Tooltip from '@/components/Tooltip/Tooltip';
-import GithubLogo from '@/icons/socials/github.svg';
-import GmailLogo from '@/icons/socials/gmail.svg';
-import TelegramLogo from '@/icons/socials/telegram.svg';
-import WhatsappLogo from '@/icons/socials/whatsapp.svg';
+import socials from '@/models/socials';
 
 import s from './Socials.scss';
 
 function Socials() {
-  const copyDataToBuffer = (clickEvent: React.MouseEvent) => {
-    const target = clickEvent.target as HTMLInputElement;
-    const data = target.value;
-
-    navigator.clipboard.writeText(data);
-  };
-
   return (
-    <address className={s.contacts}>
-      <span className={s.location}>Russia, Voronezh</span>
-
-      <div className={s.contact}>
-        <a
-          className={s.contactLink}
-          href="mailto:vasily.kovnev@gmail.com"
-          rel="noreferrer"
-          target="_blank"
-        >
-          <GmailLogo height="30px" width="30px" />
-        </a>
-        <Tooltip text="copy">
-          <input
-            className={s.contactField}
-            onClick={copyDataToBuffer}
-            readOnly
-            type="text"
-            value="vasily.kovnev@gmail.com"
-          />
-        </Tooltip>
-      </div>
-
-      <div className={s.contact}>
-        <a
-          className={s.contactLink}
-          href="tel:+79507653267"
-          rel="noreferrer"
-          target="_blank"
-        >
-          <WhatsappLogo height="35px" width="35px" />
-        </a>
-        <Tooltip text="copy">
-          <input
-            className={s.contactField}
-            onClick={copyDataToBuffer}
-            readOnly
-            type="text"
-            value="+79507653267"
-          />
-        </Tooltip>
-      </div>
-
-      <ul className={s.socials}>
-        <li>
+    <ul className={s.socials}>
+      {socials.map((social) => (
+        <li key={social.link}>
           <a
             className={s.contactLink}
-            href="https://github.com/KaguraDun"
+            href={social.link}
             rel="noreferrer"
             target="_blank"
           >
-            <GithubLogo height="40px" width="40px" />
+            {social.logo}
           </a>
         </li>
-
-        <li>
-          <a
-            className={s.contactLink}
-            href="https://t.me/KaguraDun"
-            rel="noreferrer"
-            target="_blank"
-          >
-            <TelegramLogo height="35px" width="35px" />
-          </a>
-        </li>
-      </ul>
-    </address>
+      ))}
+    </ul>
   );
 }
 
