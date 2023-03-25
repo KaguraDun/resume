83dcfcbd94b09743a0f4f0fb63a71713381f6226
Vasily Kovnev <54976219+KaguraDun@users.noreply.github.com>
2023-03-25 20:55:47 +0300

    feat: add pages model

---
+++ src/models/pages.tsx
@@ -0,0 +1,30 @@
+import About from '@/pages/About/About';
+import Projects from '@/pages/Projects/Projects';
+import WorkExperience from '@/pages/WorkExperience/WorkExperience';
+
+interface IPages {
+  linkTo: string;
+  navText: string;
+  componentToRender: JSX.Element;
+}
+
+const pages = [
+  {
+    linkTo: '/',
+    navText: 'About',
+    componentToRender: <About />,
+  },
+  {
+    linkTo: '/work',
+    navText: 'Work experience',
+    componentToRender: <WorkExperience />,
+  },
+  {
+    linkTo: '/educational',
+    navText: 'Educational Projects',
+    componentToRender: <Projects />,
+  },
+];
+
+export default pages;
+export type { IPages };
